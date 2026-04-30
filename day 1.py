0be5b05bff855d99d0f5c76d877a1c7be046d65c
pamarthimanasalakshmi99-beep <pamarthimanasalakshmi99@gmail.com>
2026-04-30 22:04:47 +0530

    Create day 1.py

---
+++ day 1.py
@@ -0,0 +1,17 @@
+# Day 1 Practice
+
+# Even or Odd
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# Largest of two numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1 > num2:
+    print("Largest is:", num1)
+else:
+    print("Largest is:", num2)
